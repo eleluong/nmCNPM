7f3502cde322ba7165e848c9e9729d5aff48d185
minhhieu <mhieu37a@gmail.com>
2022-01-13 22:34:35 +0700

    edit checkout

--- lirvecafe/src/App.js
+++ lirvecafe/src/App.js
@@ -14,6 +14,7 @@ import Products from './frontend/components/products/products'
 function App() {
 
     return (
+        <>
         <BrowserRouter >
             <Routes>
                 <Route exact path={ROUTES.HOME} element={<Home />}></Route>
@@ -25,7 +26,8 @@ function App() {
         </BrowserRouter>
 
         
-        
+        <Checkout></Checkout>
+        </>
 
         
     )
