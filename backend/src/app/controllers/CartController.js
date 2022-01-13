6330fcc4e5f60167b313e38d0be46adc31d711d7
MinhNV <93144113+MinhNguyen1001@users.noreply.github.com>
2022-01-13 14:54:52 +0700

    Update CartController.js

--- backend/src/app/controllers/CartController.js
+++ backend/src/app/controllers/CartController.js
@@ -4,94 +4,108 @@ const carts = db.collection('carts');
 class CartsController {
 
     async getCart(req, res) {
-        //const id = req.session.passport.user;
-        const id = req.params.id;
-        await carts.doc(id).get()
-        .then(cart => {
-            if(!cart.exists) {
-                res.send('chưa có gì trong giỏ hàng')
-            }
-            res.send(cart.data());
-        })
-        .catch(err => {
-            console.error('err', err);
-            res.status(400).json({ error: err.message });
-        })
+        try {
+            const cartId = req.params.cartId;
+
+            const products = (await carts.doc(cartId).collection('productList').get()).docs;
+
+            var items = products.map(function(product) {
+                return {
+                    productId: product.id,
+                    quantity: product.data().quantity
+                }
+            });
+
+            return res.status(200).send(items);
+        } catch(error) {
+            return res.sendStatus(500);
+        }
     }
 
     async deleteFromCart(req, res) {
         //const id = req.session.passport.user;
-        const id = req.body.customerId;
-        const cart = carts.doc(id);
-        await cart.collection('productList').doc(req.body.id).get()
-        .then(product => {
-            product.ref.delete().then(() => {return cart.get()});
+        const productId = req.params.productId;
+        const cartId = req.params.cartId;
+        
+        const products = (await carts.doc(cartId).collection('productList').get()).docs;
+
+        const deletingProduct = products.find(product => {
+            return product.id === productId;
         })
-        .then(doc => {
-            res.send(doc.data())
-        })
-        .catch(err => {
-            console.error('err', err);
-            res.status(400).json({ error: err.message });
-        })
+
+        let quantity = deletingProduct.data().quantity;
+        if (quantity > 1) {
+            carts.doc(cartId)
+                 .collection('productList').doc(deletingProduct.id)
+                 .set({
+                     quantity: quantity - 1
+                 });
+        } else {
+            carts.doc(cartId)
+                 .collection('productList').doc(deletingProduct.id)
+                 .delete({});
+        }
+
+        return res.sendStatus(200);
     }
 
     async addToCart(req, res) {
-        //const id = req.session.passport.user;
-        const id = req.body.customerId;
-        let doc = carts.doc(id).collection('productList').doc(req.body.id);
-        await carts.doc(id).get()
-        .then(cart => {
-            if(!cart.exists) {
-                console.log('1')
-                carts.doc(id).set({
-                    number: 1,
-                })
-            }
-            return doc.get()
-        })
-        .then(product => {
-            if(!product.exists)
-                return product.ref.set({
-                    number: 1,
-                })
-            else return product.ref.update({
-                number: product.data().number += 1,
-            })
-        })    
-        .then(() => {
-            return doc.get()
-        })
-        .then(doc => {
-            res.send(doc.data())
-        })
-        .catch(err => {
-            console.error('err', err);
-            res.status(400).json({ error: err.message });
-        })
-    }
+        const productId = req.params.productId;
+        const cartId = req.params.cartId;
+        
+        const products = (await carts.doc(cartId).collection('productList').get()).docs;
 
-    async descreaseFromCart(req, res) {
+        const addingProduct = products.find(product => {
+            return product.id === productId;
+        })
+
+        if (addingProduct) {
+            carts.doc(cartId)
+                 .collection('productList').doc(productId)
+                 .set({
+                     quantity: addingProduct.data().quantity + 1
+                 });
+        } else {
+            carts.doc(cartId)
+                 .collection('productList').doc(productId)
+                 .set({
+                     quantity: 1
+                 });
+        }
+
+        return res.sendStatus(200);
         //const id = req.session.passport.user;
-        const id = req.body.customerId;
-        const cart = carts.doc(id);
-        await cart.collection('productList').doc(req.body.id).get()
-        .then(product => {
-            if(product.data().number == 1)
-                product.ref.delete();
-            else product.ref.update({
-                number: this.number -= 1,
-            }).then(() => {
-                return cart.get()
-            })     
-        })
-        .then(doc => {
-            res.send(doc.data())
-        })
-        .catch(err => {
-            console.error('err', err);
-            res.status(400).json({ error: err.message });
-        })
+        // const id = req.body.customerId;
+        // let doc = carts.doc(id).collection('productList').doc(req.body.id);
+        // await carts.doc(id).get()
+        // .then(cart => {
+        //     if(!cart.exists) {
+        //         console.log('1')
+        //         carts.doc(id).set({
+        //             number: 1,
+        //         })
+        //     }
+        //     return doc.get()
+        // })
+        // .then(product => {
+        //     if(!product.exists)
+        //         return product.ref.set({
+        //             number: 1,
+        //         })
+        //     else return product.ref.update({
+        //         number: product.data().number += 1,
+        //     })
+        // })    
+        // .then(() => {
+        //     return doc.get()
+        // })
+        // .then(doc => {
+        //     res.send(doc.data())
+        // })
+        // .catch(err => {
+        //     console.error('err', err);
+        //     res.status(400).json({ error: err.message });
+        // })
     }
 }
 
