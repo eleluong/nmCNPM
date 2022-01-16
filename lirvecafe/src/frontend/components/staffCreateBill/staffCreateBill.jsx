3db8cd2fd5e48924ac6448f64e7cb1e9b9d8b227
chumanhtien <tiencm194182@gmail.com>
2022-01-17 00:20:48 +0700

    some changes

--- lirvecafe/src/frontend/components/staffCreateBill/staffCreateBill.jsx
+++ lirvecafe/src/frontend/components/staffCreateBill/staffCreateBill.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import Products from './products/products';
-<<<<<<< HEAD
 // import Bill from './bill/bill';
-=======
-import Bill from './Bill/bill';
->>>>>>> 3b353fbc8ff84caa37d34563aa6695d4ef5040fe
 const StaffCreateBill = () => {
     return (
         <div>
