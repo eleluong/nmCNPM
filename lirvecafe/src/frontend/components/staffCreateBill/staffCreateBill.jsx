e96660015ed6f6b22dba60d30e269f18b1153889
eleluong <tinh12345bn@gmail.com>
2022-01-16 21:53:12 +0700

    ...

---
+++ lirvecafe/src/frontend/components/staffCreateBill/staffCreateBill.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Products from './products/products';
+import Bill from './bill/bill';
+const StaffCreateBill = () => {
+    return (
+        <div>
+            <Products/>
+            <Bill/>
+        </div>
+    )
+}
+
+export default StaffCreateBill
