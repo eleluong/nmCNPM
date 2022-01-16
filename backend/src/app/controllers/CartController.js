d34b1d2a59dcad8fa862d0445c22071d6e342c16
MinhHieu <95637579+minhhieu184@users.noreply.github.com>
2022-01-17 00:36:55 +0700

    add user, creat bill

--- backend/src/app/controllers/CartController.js
+++ backend/src/app/controllers/CartController.js
@@ -50,11 +50,29 @@ class CartController {
         return res.status(200);
     }
 
+    async checkIfExists(req, res, next) {
+        const id = req.body.cartId;
+        await carts.doc(id).get()
+        .then(cart => {
+            if(!cart.exists) {
+                console.log('1')
+                carts.doc(id).set({
+                    number: 1,
+                })
+            }
+            next();
+        })
+        .catch(err => {
+            console.error('err', err);
+            res.status(400).json({ error: err.message });
+        })
+    }
+
+
     //PUT
     async addToCart(req, res) {
         const productId = req.body.productId;
         const cartId = req.body.cartId;
-        const price = req.body.price;
 
         const products = (await carts.doc(cartId).collection('productList').get()).docs;
 
@@ -65,15 +83,14 @@ class CartController {
         if (addingProduct) {
             carts.doc(cartId)
                 .collection('productList').doc(productId)
-                .update({
+                .set({
                     quantity: addingProduct.data().quantity + 1
                 });
         } else {
             carts.doc(cartId)
                 .collection('productList').doc(productId)
                 .set({
-                    quantity: 1,
-                    price: price
+                    quantity: 1
                 });
         }
 
@@ -110,23 +127,6 @@ class CartController {
         //     console.error('err', err);
         //     res.status(400).json({ error: err.message });
         // })
-    }
-
-    //GET
-    async getTotal(req, res) {
-        try {
-            const cartId = req.params.cartId;
-
-            const products = (await carts.doc(cartId).collection('productList').get()).docs;
-
-            const sum = products.reduce((total, product) => {
-                return total + product.data().quantity*product.data().price;
-            }, 0); 
-
-            return res.status(200).json(sum);
-        } catch(error) {
-            return res.status(500);
-        }
     }
 }
 
