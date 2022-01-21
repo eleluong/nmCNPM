d8d9092710f75a05c4daeafb68ef8b83a19faca6
MinhHieu <95637579+minhhieu184@users.noreply.github.com>
2022-01-21 10:56:31 +0700

    fix header

--- lirvecafe/src/frontend/components/constants/userCookie.js
+++ lirvecafe/src/frontend/components/constants/userCookie.js
@@ -15,6 +15,7 @@ function getCookie(cname) {
 }
 
 function setCookie(cname, cvalue, exdays) {
+    console.log('xxxxxxxxxxx');
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
